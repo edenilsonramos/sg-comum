8e1cd95b8f5d7e21da990e9b8591eff2e70c29d4
Edenilson Ramos <edenilson.ramos18@gmail.com>
2023-07-13 19:53:32 -0300

    fix: upando coisas que esqueci

--- Utils/Constants.cs
+++ Utils/Constants.cs
@@ -9,7 +9,7 @@ namespace SGComum.Utils
 
         public const string URLStageAmoOfertas = "https://api.stage.amo.delivery/";
 
-        public const string TOKEN_AMO = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiTWFyY29zIEdpb3JkYW5pIiwiZW1haWwiOiJxdWFsaWRhZGVAc2dici5jb20uYnIiLCJ0b2tlbl9pZCI6IjY0NDA0ODZiMWFkMTA2MDAxYTBjNDFmNyIsImNsaWVudF90eXBlIjoicmVzdCIsInNjb3BlIjoiZXN0YWJsaXNobWVudCIsImVzdGFibGlzaG1lbnRfaWQiOiI2NDNkNzY5MTQxNzEyMDAwMWFmOTkwZWMiLCJpYXQiOjE2ODE5MzQ0NDN9.6E35UzVsoQhyz7_y10tsi3YdE1VwwegYJL9gR1SDJH0";
+        public const string TOKEN_AMO = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiU0dCciIsImVtYWlsIjoiZGVzZW52b2x2aW1lbnRvM0BzZ2JyLmNvbSIsInRva2VuX2lkIjoiNjEwODM1ZGE2OWVkY2EwMDEzODRiYzYwIiwiY2xpZW50X3R5cGUiOiJyZXN0Iiwic2NvcGUiOiJlc3RhYmxpc2htZW50IiwiZXN0YWJsaXNobWVudF9pZCI6IjVmYzAwM2ZmOThkN2YyMDAyM2Q2ZTc1NyIsImlhdCI6MTYyNzkyODAyNn0.IeuNfqyQ_4EtBtPDXLq4KUaGIP-jWQFVnGbOebqZhb0";
 
         public const string TOKEN_MEUSG = "5L/8K//}#$@dwKgf/)XqqQB6ZnrhFHX4qm[Y*=wX&%sxSvU5S;nsBm&U=K=7";
 
