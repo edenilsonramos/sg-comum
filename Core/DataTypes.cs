1825a7c94550c866f73954af3e5ac76b22a2db48
edenilsonramos <edenilson.ramos18@gmail.com>
2023-07-14 17:16:48 -0300

    feat: upando porque tinha sido mexido e nao lembro em que kkk

---
+++ Core/DataTypes.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SGComum.Core
+{
+    public static class DataTypes
+    {
+        public enum AmoOfertasOrderStatus
+        {
+            sNew,
+            sConfirmed, 
+            sCanceled, 
+            sCompleted, 
+            sScheduled, 
+            sLocal, 
+            sSent, 
+            sAwaitingPayment
+        }
+    }
+}
